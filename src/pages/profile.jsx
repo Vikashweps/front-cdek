790021872413eaa8041b794b415cc3a496237189
Anastasia <pomelova-2004@mail.ru>
2026-03-23 16:32:50 +0700

    визуал страницы регистрации и профиля

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,13 +1,68 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import './profile.css';
 
 function Profile() {
   return (
-    <div>
-      <h1>Профиль пользователя</h1>
-      {/* Ваш контент */}
+    <div className="overlay2">
+      <div className="card2">
+        <h1>МОЙ ПРОФИЛЬ</h1>
+
+        {/* Контейнер для двух колонок */}
+        <div className="profile-content">
+          
+          <div className="column personal-data">
+            <h3>ЛИЧНЫЕ ДАННЫЕ</h3>
+            <form>
+              <div className="input-readonly">
+                <span className="value">Иван Иванов</span>
+                </div>
+
+                <div className="input-readonly">
+                <span className="value">ivan@example.com</span>
+                </div>
+
+                <button type="button" className="sec">
+              
+                ИЗМЕНИТЬ ДАННЫЕ
+              </button>
+
+             <button type="button" className="prim">
+                ВИШЛИСТ
+              </button>
+
+              <button type="button" className="sec">
+                НА ГЛАВНУЮ
+              </button>
+            </form>
+          </div>
+
+          <div className="column games-list">
+            <h3>МОИ ИГРЫ</h3>
+            
+            {/* Контейнер со скроллом для кнопок команд */}
+            <div className="games-scroll-container">
+              <button type="button" className="game-item">
+                Команда 1
+              </button>
+
+              <button type="button" className="game-item">
+                Команда 2
+              </button>
+              
+              <button type="button" className="game-item">Команда 3</button>
+              <button type="button" className="game-item">Команда 4</button>
+              <button type="button" className="game-item">Команда 5</button>
+              <button type="button" className="game-item">Команда 6</button>
+              <button type="button" className="game-item">Команда 7</button>
+              <button type="button" className="game-item">Команда 8</button> 
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+
+export default Profile;
--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './registration.css';
 
 function Registration() {
   return (
-    <div>
-      <h1>Регистрация</h1>
-      {/* Ваш контент */}
+    <div className="overlay1">
+      <div className="card1">
+        <h1>ТАЙНЫЙ САНТА</h1>
+
+        <form>
+          <input type="text" placeholder="Введите имя" />
+          <input type="email" placeholder="Введите почту" />
+
+          <button className="primary1" type="submit1">
+            Создать аккаунт
+          </button>
+
+          <button type="button" className="secondary1">
+            Авторизоваться через Github
+          </button>
+        </form>
+
+        <p className="agreement1">
+          Продолжая, вы соглашаетесь с Пользовательским соглашением
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Registration
+export default Registration;
