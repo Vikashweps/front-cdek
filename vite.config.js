524d9460b42c9c8bcd63abfcb922642a9111b0a1
Vika <ia331s30@csc.sibsutis.ru>
2026-04-12 11:00:46 +0700

    add port

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,22 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-})
+})*/
 
-/*export default defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Адрес твоего бэкенда
+        target: 'http://localhost:3000', // Адрес бэкенда
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})*/
+})
